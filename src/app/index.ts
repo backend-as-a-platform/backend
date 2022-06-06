816c0db70f4066a55a83eeb126a1fb42f7de4eca
arjunshibu <arjunshibu1999@gmail.com>
2022-06-06 10:12:51 +0530

    updated CORS policy

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,12 @@
 import * as express from 'express';
 import * as cors from 'cors';
-import config from './config';
+// import config from './config';
 import api from '../router/api';
 import error from '../middlewares/error';
 
 const app = express();
-const corsOrigin = cors({ origin: config('FRONTEND_URI') });
+// const corsOrigin = cors({ origin: config('FRONTEND_URI') });
+const corsOrigin = cors({ origin: '*' });
 
 app.use(express.json());
 app.use(corsOrigin);
